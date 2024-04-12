69db4797f2a639d5770a4f0025f2ec48bdfb6242
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-04-12 11:36:57 +0100

    Update Unix

--- Useful_Genomics_Codes/1_gff_to_fasta.sh
+++ Useful_Genomics_Codes/1_gff_to_fasta.sh
@@ -1,6 +1,16 @@
 for file in *.gff; do
-        seqret -sequence "$file" -outseq "${file%.*}.fasta" -osformat fasta
+  name=${file%.gff} 
+  grep -A999999 "##FASTA" $file | grep -v "##FASTA" >> ./${name}.fasta
 done
 
+
+# For managing gff in some folders & creating many FASTA files from a gff, see:
+# https://www.microbialsystems.cn/en/post/gff3tofasta/
+
+# Seqret by EMBOSS:
 # SOURCE: http://emboss.open-bio.org/html/use/ch06s01.html
 # SOURCE: https://askubuntu.com/questions/1342080/convert-multiple-files-in-one-command
+
+# Some notes:
+# gff files (General Feature Format) has already contains FASTA with annotations.
+# The only thing that is reasonable is to extract the FASTA-part of the gff files and collect it in a single FASTA file output.
