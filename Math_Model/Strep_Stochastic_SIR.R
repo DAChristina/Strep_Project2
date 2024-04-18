b7c45f96e631e21a91560846cb34132b838847af
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-04-18 17:25:42 +0100

    Create Strep_Stochastic_SIR.R

---
+++ Math_Model/Strep_Stochastic_SIR.R
@@ -0,0 +1,15 @@
+if (!require(odin.dust, quietly=T)){
+  install.packages("odin.dust",
+  repos = c("https://mrc-ide.r-universe.dev", "https://cloud.r-project.org"))
+
+  library(odin.dust)
+}
+
+transition <- odin.dust::odin({
+  x
+  
+
+  
+})
+                          
+  
