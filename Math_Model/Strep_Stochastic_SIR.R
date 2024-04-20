ad1aeca7a82bd6b3dc741af6e1388a032c7021cc
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-04-20 15:22:08 +0100

    Stochastic failed to run

--- Math_Model/Strep_Stochastic_SIR.R
+++ Math_Model/Strep_Stochastic_SIR.R
@@ -16,13 +16,15 @@ transition <- odin.dust::odin_dust({
   beta <- user()
   sigma <- user()
   
+  
+  n_SI <- rnbinom(S, 1 - exp(-beta * I / N))
+  n_IR <- rnbinom(I, 1 - exp(-sigma))
+  
 # 2. INITIAL VALUES ############################################################
   initial(S) <- N - I0
   initial(I) <- I0
   initial(R) <- 0
   
-  n_SI <- rnbinom(S, 1 - exp(-beta * I / N))
-  n_IR <- rnbinom(I, 1 - exp(-sigma))
 
 # 3. UPDATES ###################################################################
   update(S) <- S - n_SI
@@ -37,5 +39,5 @@ pars <- list(N = 10e6,
              sigma = 1)
 
 mod <- transition$new(user = pars) # changing the cycle by user loops instead of define the cycle_width one-by-one
-timesteps <- seq(0, 2000, by=1)   # time.
-y <- mod$run(timesteps)
+plot(mod$run(0:100), type = "s")
+lines(t_capacity, y_capacity, type = "s", col = "red", lty = 2)
