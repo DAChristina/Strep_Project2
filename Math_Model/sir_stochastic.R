bb7bcdf8712fec1a3a75067f09418d5bfb4e6d1b
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-04-23 15:07:34 +0100

    Poisson but need investigation

--- Math_Model/sir_stochastic.R
+++ Math_Model/sir_stochastic.R
@@ -1,8 +1,9 @@
 
+
 # To change the model into stochastic fuction, use the probabilistic function:
 ## Individual probabilities of transition:
 ## Definition of the time-step and output as "time"
-dt <- user(1)
+dt <- user()
 initial(time) <- 0
 update(time) <- (step + 1) * dt
 
@@ -21,23 +22,26 @@ initial(R) <- 0
 
 # 3. UPDATES ###################################################################
 N <- S + I + R
-beta <- (R0*I)/(N*DOI)
+lambda <- (R0*I)/(N*DOI)
 
 # Individual probabilities of transition
-p_SI <- 1- exp(-beta * dt)
+p_SI <- 1- exp(-lambda * dt)
 p_IR <- 1- exp(-sigma * dt)
 
 # Draws for numbers changing between compartments
-temp_n_SI <- rbinom(S, p_SI) # contrasting to only rbinom(S, p_SI), need correction
-temp_n_IR <- rbinom(I, p_IR) # contrasting to only rbinom(I, p_IR), need correction
+# temp_n_SI <- rbinom(S, p_SI) # contrasting to only rbinom(S, p_SI), need correction
+n_IR <- rbinom(I, p_IR) # contrasting to only rbinom(I, p_IR), need correction
 # size is the overdispersion parameter k, smaller k -> greater variance
 # a check to ensure there are not more incident infections than susceptible individuals
+# temp_n_SI <- rnbinom(R, R0*S/N)
+# n_SI <- if (n_IR > 0) min(temp_n_SI, S)  else 0 # Check if the result < 0, throw 0
 
-n_SI <- if (temp_n_IR > 0) sum(rnbinom(temp_n_SI, p_SI))  else 0 # Check if the result < 0, throw 0
+n_SI <- if (n_IR > 0) min(rpois(R), S)  else 0
+
+
 # Error: Expected 2 arguments in rnbinom call, but recieved 3
 # Error: Argument to sum must be a symbol or indexed array
 # sum(rnbinom(n=recovery, mu = R0*data$S[i-1]/N, size = k))
-n_IR <- temp_n_IR
 
 update(S) <- S - n_SI
 update(I) <- I + n_SI - n_IR
