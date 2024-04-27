4184e2e1a8a744a7c70aa94b17cf95a86c38195c
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-04-27 15:27:39 +0100

    basic closed system with beta & gamma

--- Math_Model/sir_stochastic.R
+++ Math_Model/sir_stochastic.R
@@ -10,10 +10,8 @@ update(time) <- (step + 1) * dt
 # 1. PARAMETERS ################################################################
 S_ini <- user()
 I_ini <- user()
-R0 <- user()
-DOI <- user()
+beta <- user()
 sigma <- user()
-# kappa <- user() # overdispersion parameter, with smaller k -> greater variance
 
 # 2. INITIAL VALUES ############################################################
 initial(S) <- S_ini
@@ -22,27 +20,17 @@ initial(R) <- 0
 
 # 3. UPDATES ###################################################################
 N <- S + I + R
-lambda <- (R0*I)/(N*DOI)
+lambda <- beta*I/N
 
 # Individual probabilities of transition
 p_SI <- 1- exp(-lambda * dt)
 p_IR <- 1- exp(-sigma * dt)
 
 # Draws for numbers changing between compartments
-# temp_n_SI <- rbinom(S, p_SI) # contrasting to only rbinom(S, p_SI), need correction
-n_IR <- rbinom(I, p_IR) # contrasting to only rbinom(I, p_IR), need correction
-# size is the overdispersion parameter k, smaller k -> greater variance
-# a check to ensure there are not more incident infections than susceptible individuals
-# temp_n_SI <- rnbinom(R, R0*S/N)
-# n_SI <- if (n_IR > 0) min(temp_n_SI, S)  else 0 # Check if the result < 0, throw 0
+n_SI <- rbinom(S, p_SI)
+n_IR <- rbinom(I, p_IR)
 
-n_SI <- if (n_IR > 0) min(rpois(R), S)  else 0
-
-
-# Error: Expected 2 arguments in rnbinom call, but recieved 3
-# Error: Argument to sum must be a symbol or indexed array
-# sum(rnbinom(n=recovery, mu = R0*data$S[i-1]/N, size = k))
-
+# The transitions
 update(S) <- S - n_SI
 update(I) <- I + n_SI - n_IR
 update(R) <- R + n_IR
