a3ee6507cfc2622ffbe82bd8354a77d3e5e6d19e
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-04-28 11:54:58 +0100

    column correction

--- Math_Model/sir_stochastic_odin.R
+++ Math_Model/sir_stochastic_odin.R
@@ -12,9 +12,9 @@ if (!require(dde, quietly=T)){
 }
 
 gen_sir <- odin::odin({
-  # dt <- user()
-  # initial(time) <- 0
-  # update(time) <- (step + 1) * dt
+  dt <- user()
+  initial(time) <- 0
+  update(time) <- (step + 1) * dt
   
   # 1. PARAMETERS ################################################################
   S_ini <- user()
@@ -32,8 +32,8 @@ gen_sir <- odin::odin({
   lambda <- beta*I/N
   
   # Individual probabilities of transition
-  p_SI <- 1- exp(-lambda) # assume dt equals to 1
-  p_IR <- 1- exp(-sigma) # assume dt equals to 1
+  p_SI <- 1- exp(-lambda * dt) # assume dt equals to 1
+  p_IR <- 1- exp(-sigma * dt) # assume dt equals to 1
   
   # Draws for numbers changing between compartments
   n_SI <- rbinom(S, p_SI)
@@ -46,25 +46,30 @@ gen_sir <- odin::odin({
 })
 
 # sir_model$state()
-all_date <- data.frame(date = seq(1:4745)) # is number of day of observations
+# all_date <- data.frame(date = seq(1:4745)) # is number of day of observations
 
-pars <- list(#dt = 1,
-             S_ini = 1e5,
-             I_ini = 10,
-             beta = 0.2,
-             sigma = 0.1
-             # DOI = 15.75, # 15.75 days (95% CI 7.88-31.49) (Serotype 1) (Chaguza et al., 2021)
+pars <- list(dt = 1,
+  S_ini = 1e5,
+  I_ini = 10,
+  beta = 0.2,
+  sigma = 0.1
+  # DOI = 15.75, # 15.75 days (95% CI 7.88-31.49) (Serotype 1) (Chaguza et al., 2021)
 )
 
 sir_model <- gen_sir$new(user = pars)
 
 set.seed(0)
 timesteps <- seq(0, nrow(all_date), by=1)   # time.
-sir_res <- sir_model$run(0:nrow(all_date))
+# timesteps <- seq(0, 500, by=1)   # trial other timestep value to compare the result
+sir_res <- sir_model$run(timesteps)
 
 par(mar = c(4.1, 5.1, 0.5, 0.5), las = 1)
 cols <- c(S = "#8c8cd9", I = "#cc0044", R = "#999966")
 
-matplot(sir_res[, 1], sir_res[, -1], xlab = "Time", ylab = "Number of individuals",
+matplot(sir_res[, 1], sir_res[, (3:ncol(sir_res))], # not choose the first & second column
+        xlab = "Time", ylab = "Number of individuals",
         type = "l", col = cols, lty = 1)
-legend("topleft", lwd = 1, col = cols, legend = c("S", "I", "R"), bty = "n")
+legend("topright", lwd = 1, col = cols, legend = c("S", "I", "R"), bty = "n")
+
+# The sir_res output is a matrix, save this to csv:
+write.csv(sir_res, file="Output_sir_result.csv", row.names =T)
