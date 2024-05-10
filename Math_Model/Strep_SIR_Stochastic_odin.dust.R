c7eddc5a65f0ffe0d6eeb763cacbb369f9d2a929
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-05-10 12:29:07 +0100

    fixed parms 1 of SADR+seasonality

--- Math_Model/Strep_SIR_Stochastic_odin.dust.R
+++ Math_Model/Strep_SIR_Stochastic_odin.dust.R
@@ -19,17 +19,18 @@ wd = "C:/Users/dac23/Downloads" # library computers
 wd = "/home/ron/Downloads" # personal OSs
 setwd(wd)
 
-library(odin.dust)
 gen_sir <- odin.dust::odin_dust("sir_stochastic.R")
 
 # Running the SIR model with dust
 pars <- list(dt = 1,
              S_ini = 6e7, # England's pop size is roughly 67,000,000
-             A_ini = 0,
-             I_ini = 5,
-             beta = (1/15.75)*5.9, # based on mcmc run, prior = (1/15.75)*1.2,
-	     delta = (1/15.75)*0.03, # incubation period, trial random number
-             sigma = (1/15.75) # carriage duration = 15.75 days (95% CI 7.88-31.49) (Serotype 1) (Chaguza et al., 2021)
+             A_ini = 100,
+             D_ini = 0,
+             beta_0 = 0.25, # based on mcmc run, prior = (1/15.75)*1.2,
+             beta_1 = 0.8,
+             delta = (1/60), # incubation period, trial random number
+             qu = 0.23, # When clinical symptoms occur, (1-qu) = no clinical symptoms detected
+             sigma = (1/15.75) # carriage duration of diseased = 15.75 days (95% CI 7.88-31.49) (Serotype 1) (Chaguza et al., 2021)
 ) # Serotype 1 is categorised to have the lowest carriage duration
 
 sir_model <- gen_sir$new(pars = pars,
@@ -59,17 +60,21 @@ library(tidyverse)
 glimpse(x)
 
 par(mar = c(5.1, 5.1, 0.5, 0.5), mgp = c(3.5, 1, 0), las = 1)
-cols <- c(S = "#8c8cd9", A = "darkred", I = "#cc0099", R = "#999966", n_AI_daily = "orange", n_AI_cumul = "green")
-matplot(time, t(x[1, , ]), type = "l",
-        xlab = "Time", ylab = "Number of individuals",
-        col = cols[["S"]], lty = 1, ylim = range(x))
-matlines(time, t(x[2, , ]), col = cols[["A"]], lty = 1)
-matlines(time, t(x[3, , ]), col = cols[["I"]], lty = 1)
-matlines(time, t(x[4, , ]), col = cols[["R"]], lty = 1)
-matlines(time, t(x[5, , ]), col = cols[["n_AI_daily"]], lty = 1)
-matlines(time, t(x[6, , ]), col = cols[["n_AI_cumul"]], lty = 1)
+cols <- c(S = "#8c8cd9", A = "darkred", D = "#cc0099", R = "#999966", n_AD_daily = "orange", n_AD_cumul = "green")
+# matplot(time, t(x[1, , ]), type = "l",
+#         xlab = "Time", ylab = "Number of individuals",
+#         col = cols[["S"]], lty = 1, ylim = range(x))
+matplot(time, t(x[2, , ]), type = "l",
+                xlab = "Time", ylab = "Number of individuals",
+                col = cols[["A"]], lty = 1)#, ylim = max(x[2,,]))
+# matlines(time, t(x[2, , ]), col = cols[["A"]], lty = 1)
+matlines(time, t(x[3, , ]), col = cols[["D"]], lty = 1)
+# matlines(time, t(x[4, , ]), col = cols[["R"]], lty = 1)
+# matlines(time, t(x[5, , ]), col = cols[["n_AD_daily"]], lty = 1)
+# matlines(time, t(x[6, , ]), col = cols[["n_AD_cumul"]], lty = 1)
 legend("left", lwd = 1, col = cols, legend = names(cols), bty = "n")
-max(x[5,,]) # Check max n_AI_daily
-max(x[3,,]) # Check max I
+max(x[5,,]) # Check max n_AD_daily
+max(x[3,,]) # Check max D
+pars$beta_0/(pars$delta) + (pars$qu*pars$delta)/(pars$delta*pars$sigma) # print R0
 
 write.csv(x, file="Output_sir_result.csv", row.names =T)
