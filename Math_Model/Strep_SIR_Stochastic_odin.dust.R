ea851b192be3952f1e8c28ad3ae70aa715200a46
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-05-17 16:01:12 +0100

    Competing hazard correction

--- Math_Model/Strep_SIR_Stochastic_odin.dust.R
+++ Math_Model/Strep_SIR_Stochastic_odin.dust.R
@@ -24,22 +24,20 @@ gen_sir <- odin.dust::odin_dust("sir_stochastic.R")
 # Running the SIR model with dust
 pars <- list(dt = 1,
              S_ini = 6e7, # England's pop size is roughly 67,000,000
-             A_ini = 100,
+             A_ini = 10,
              D_ini = 0,
-             beta_0 = 0.01,
-             beta_1 = 0.9,
-             vacc = 0.9*0.862, # Infant vaccination coverage in UK * 86.2% protection efficacy
-             # for +6PCV13 only, assume 2 PCV13 doses for infant age 4-11 months
-             # (https://academic.oup.com/cid/article/73/7/e1423/6042567?login=true)
-             delta = (1/2000), # Previously assumed 90 days, lit study suggest 2000 days required for acquisition
+             time_shift = 70,
+             beta_0 = 0.1,
+             beta_1 = 0.6,
+             delta = (1/1000), # Probability of people developing disease
              # (https://academic.oup.com/jid/article/206/7/1020/806330)
-             qu = 0.0002, # Probability when clinical symptoms occur, (1-qu) = no clinical symptoms detected
-             sigma = (1/15.75) # carriage duration of diseased = 15.75 days (95% CI 7.88-31.49) (Serotype 1) (Chaguza et al., 2021)
+             sigma_1 = (1/15.75), # fixed per-day, carriage duration (95% CI 7.88-31.49) (Serotype 1) (Chaguza et al., 2021)
+             sigma_2 = (1/1) # assumed as acute phase
 ) # Serotype 1 is categorised to have the lowest carriage duration
 
 sir_model <- gen_sir$new(pars = pars,
                          time = 1,
-                         n_particles = 1L,
+                         n_particles = 10L,
                          n_threads = 4L,
                          seed = 1L)
 
@@ -55,6 +53,21 @@ n_times <- 4745 # 4745 or similar to the number of date range (of the provided d
 n_particles <- 10
 x <- array(NA, dim = c(sir_model$info()$len, n_particles, n_times))
 
+# Beta check
+time <- seq(1, n_times, 1)
+time_shift <- 70
+beta <- pars$beta_0*(1+pars$beta_1*sin(2*pi*(time_shift+time)/365))
+max(beta)
+min(beta)
+
+# R0 estimation (R0 changes due to seasonality)
+R0 <- (beta/(pars$delta+pars$sigma_1)) +  ((pars$delta)*(beta)) / ((pars$delta + 192/(4064*4745))*(pars$sigma_2 + 192/(4064*4745))) # print R0
+max(R0)
+min(R0)
+# plot(time, R0)
+# pars$beta_1/(pars$delta) + (pars$qu*pars$delta)/(pars$delta*pars$sigma) # print R0
+
+
 for (t in seq_len(n_times)) {
   x[ , , t] <- sir_model$run(t)
 }
@@ -69,10 +82,10 @@ cols <- c(S = "#8c8cd9", A = "darkred", D = "#cc0099", R = "#999966", n_AD_daily
 #         xlab = "Time", ylab = "Number of individuals",
 #         col = cols[["S"]], lty = 1, ylim = range(x))
 matplot(time, t(x[3, , ]), type = "l",
-                xlab = "Time", ylab = "Number of individuals",
-                col = cols[["D"]], lty = 1)#, ylim = max(x[2,,]))
+        xlab = "Time", ylab = "Number of individuals",
+        col = cols[["D"]], lty = 1)#, ylim = max(x[2,,]))
 # matlines(time, t(x[2, , ]), col = cols[["A"]], lty = 1)
-matlines(time, t(x[3, , ]), col = cols[["D"]], lty = 1)
+# matlines(time, t(x[3, , ]), col = cols[["D"]], lty = 1)
 # matlines(time, t(x[4, , ]), col = cols[["R"]], lty = 1)
 # matlines(time, t(x[5, , ]), col = cols[["n_AD_daily"]], lty = 1)
 # matlines(time, t(x[6, , ]), col = cols[["n_AD_cumul"]], lty = 1)
@@ -80,14 +93,7 @@ legend("left", lwd = 1, col = cols, legend = names(cols), bty = "n")
 max(x[5,,]) # Check max n_AD_daily
 max(x[3,,]) # Check max D
 
-# R0 estimation (R0 changes due to seasonality)
-time <- n_times
-time_shift <- 70
-R0 <- pars$beta_0*(1+pars$beta_1*sin(2*pi*(time_shift+time)/365))/(pars$delta) + (pars$qu*pars$delta)/(pars$delta*pars$sigma) # print R0
-max(R0)
-min(R0)
-# plot(time, R0)
-# pars$beta_1/(pars$delta) + (pars$qu*pars$delta)/(pars$delta*pars$sigma) # print R0
+
 
 
 write.csv(x, file="Output_sir_result.csv", row.names =T)
